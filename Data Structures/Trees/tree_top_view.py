048df6bccdacdc3182ad1fa6d0c7e6e3b0f082db
Nilton Gomes Martins Junior <nilton.junior51@hotmail.com>
2019-01-22 16:35:08 -0200

    Update

---
+++ Data Structures/Trees/tree_top_view.py
@@ -0,0 +1,64 @@
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.info (the value of the node)
+"""
+# TODO: Correct and understand better the following proposed solution
+
+def topView(root, side=0):
+    if root is None:
+        return 0
+    if side == 0 or side == -1:
+        topView(root.left, side=-1)
+    print(root.info, end=" ")
+    if side == 0 or side == +1:
+        topView(root.right, side=+1)
+    return
+    
+
+tree = BinarySearchTree()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.create(arr[i])
+
+topView(tree.root)
